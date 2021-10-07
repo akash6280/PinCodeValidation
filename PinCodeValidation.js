537d55bbf2c0956132e9794547b9e862a4bcb627
Akash Singh <akashsingh6280@gmail.com>
2021-10-08 01:59:31 +0530

    ADDED: method to check if pin code is 6 digit or not using regex

---
+++ PinCodeValidation.js
@@ -0,0 +1,14 @@
+
+function validatePincode(pincode){
+    let pinCodeRegex = RegExp("[1-9][0-9]{5}");
+    if(pinCodeRegex.test(pincode))
+        console.log("Valid Pincode");
+    else
+        throw 'Invalid pincode';
+}
+
+try{
+    validatePincode("400088");
+}catch(e){
+    console.error(e);
+}
