47e934fb1dd2bf98f504e2cac34e3baa9bba450e
Akash Singh <akashsingh6280@gmail.com>
2021-10-08 02:01:50 +0530

    MODIFIED: restricting the alphabets at starting of pincode

--- PinCodeValidation.js
+++ PinCodeValidation.js
@@ -1,6 +1,6 @@
 
 function validatePincode(pincode){
-    let pinCodeRegex = RegExp("[1-9][0-9]{5}");
+    let pinCodeRegex = RegExp("^[1-9][0-9]{5}");
     if(pinCodeRegex.test(pincode))
         console.log("Valid Pincode");
     else
@@ -9,6 +9,11 @@ function validatePincode(pincode){
 
 try{
     validatePincode("400088");
+}catch(e){
+    console.error(e);
+}
+try{
+    validatePincode("A400088");
 }catch(e){
     console.error(e);
 }
